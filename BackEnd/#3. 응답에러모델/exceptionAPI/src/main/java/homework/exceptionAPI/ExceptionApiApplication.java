2bb2bc2adfa91f2683742d4608377ac317dd40f8
HJunng <guswjd1gh@gmail.com>
2023-10-17 16:55:32 +0900

    예외처리 - DTO 생성

---
+++ BackEnd/#3. 응답에러모델/exceptionAPI/src/main/java/homework/exceptionAPI/ExceptionApiApplication.java
@@ -0,0 +1,13 @@
+package homework.exceptionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionApiApplication.class, args);
+	}
+
+}
